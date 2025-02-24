35720d1cdaf2c9fcae8c8b005792db9cd3e40dc7
Jgcbitj <joao.boemer@edu.univali.br>
2025-02-24 19:38:46 -0300

    Changed movement and added phone camera detection

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,40 +1,24 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-	[SerializeField] private InputAction pressed, axis;
-	
-	private Transform cam;
+	private Touch touch;
 	[SerializeField] private float speed = 1;
-	[SerializeField] private bool inverted;
     Rigidbody rb;
-	private Vector2 rotation;
-	private bool rotateAllowed;
 	private void Awake() 
 	{
         rb = GetComponent<Rigidbody>();
-		cam = Camera.main.transform;
-		pressed.Enable();
-		axis.Enable();
-		pressed.performed += _ => { StartCoroutine(Move()); };
-		pressed.canceled += _ => { rotateAllowed = false; };
-		axis.performed += context => { rotation = context.ReadValue<Vector2>(); };	
 	}
 
-	private IEnumerator Move()
-	{
-		rotateAllowed = true;
-		while(rotateAllowed)
+    private void Update()
+    {
+		if(Input.touchCount > 0)
 		{
-			// apply rotation
-			rotation *= speed;
-            rb.AddForce(rotation);
-			// transform.Translate(Vector3.left * speed, Space.World);
-            Debug.Log(rotation);
-			// transform.Translate(cam.right * (inverted? -1: 1), rotation.y, Space.World);
-			yield return null;
-		}
-	}
+			touch = Input.GetTouch(0);
 
+			if(touch.phase == UnityEngine.TouchPhase.Moved)
+			{
+				transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * speed, transform.position.y, transform.position.z);
+			}
+		}
+    }
 }
